9ada74df8aeaece7454bc5c13b6a1ceaa1ec918a
olegopro <evilgazz@gmail.com>
2021-05-21 08:48:18 +0300

    Подключил iFrameLoader и рекурсивно выбрал все ноды у которых есть текст с добавлением атрибута contenteditable

--- app/src/Components/editor/editor.js
+++ app/src/Components/editor/editor.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import React, {Component} from 'react'
+import '../../helpers/iframeLoader.js'
 
 export default class Editor extends Component {
     constructor() {
         super(undefined)
+
+        this.currentPage = 'index.html'
 
         this.state = {
             pageList: [],
@@ -12,8 +15,44 @@ export default class Editor extends Component {
         this.createNewPage = this.createNewPage.bind(this)
     }
 
-    componentDidMount() {
+    init(page) {
+        this.iframe = document.querySelector('iframe')
+        this.open(page)
         this.loadPageList()
+    }
+
+    open(page) {
+        this.currentPage = `../${page}`
+        this.iframe.load(this.currentPage, () => {
+            const body = this.iframe.contentDocument.body
+            let textNodes = []
+
+            function recursy(element) {
+                element.childNodes.forEach(node => {
+
+                    if (node.nodeName === '#text' && node.nodeValue.replace(/\s+/g, '').length > 0) {
+                        textNodes.push(node)
+                    } else {
+                        recursy(node)
+                    }
+                })
+            }
+
+            recursy(body)
+            textNodes.forEach(node => {
+                const wrapper = this.iframe.contentDocument.createElement('text-editor')
+                node.parentNode.replaceChild(wrapper, node)
+                wrapper.appendChild(node)
+                wrapper.contentEditable = 'true'
+            })
+
+
+        })
+
+    }
+
+    componentDidMount() {
+        this.init(this.currentPage)
     }
 
     loadPageList() {
@@ -37,23 +76,25 @@ export default class Editor extends Component {
     }
 
     render() {
-        const {pageList} = this.state
-        const pages = pageList.map((page, i) => {
-            return (
-                <h1 key={i}>{page}
-                    <a onClick={() => this.deletePage(page)} href="#">(x)</a>
-                </h1>
-            )
-        })
-        return (
-            <>
-                <input onChange={(e) => {
-                    this.setState({newPageName: e.target.value})
-                }} type="text"/>
+        // const {pageList} = this.state
+        // const pages = pageList.map((page, i) => {
+        //     return (
+        //         <h1 key={i}>{page}
+        //             <a onClick={() => this.deletePage(page)} href="#">(x)</a>
+        //         </h1>
+        //     )
+        // })
 
-                <button onClick={this.createNewPage}>Создать страницу</button>
-                {pages}
-            </>
+        return (
+            <iframe src={this.currentPage} frameBorder="0"/>
+            // <>
+            //     <input onChange={(e) => {
+            //         this.setState({newPageName: e.target.value})
+            //     }} type="text"/>
+            //
+            //     <button onClick={this.createNewPage}>Создать страницу</button>
+            //     {pages}
+            // </>
         )
     }
 }
